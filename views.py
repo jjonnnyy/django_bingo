c9de405cfc6d79df581e4668b58d9ff38cab49e2
Jonathan Ely <me@jonathanely.com>
2013-04-13 19:16:42 +0100

    Basic template up and running.

--- views.py
+++ views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from bingo.models import BingoItem
+
+def build_items_list():
+    return BingoItem.objects.all()
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    context = {'bingo_items_list': build_items_list()}
+    return render(request, 'bingo/index.html', context)
