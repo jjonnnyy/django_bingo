19eedba4c43c1e43d514fd9192d8b8dfc0e13c6c
Jonathan Ely <me@jonathanely.com>
2013-04-13 19:27:32 +0100

    Random Sampling working.

--- views.py
+++ views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from random import sample
+
 from bingo.models import BingoItem
 
 def build_items_list():
-    return BingoItem.objects.all()
-
+    items = BingoItem.objects.all()
+    return sample(list(items), 25)
 
 def index(request):
     context = {'bingo_items_list': build_items_list()}
